aff2f246a1b5121686f0963d54ba760448a138f2
Foridul35962 <foridulislam35962@gmail.com>
2025-10-28 01:15:32 +0600

    deployment config add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-      '/api': 'http://localhost:3000'
+      '/api': 'https://authentication-server-side.vercel.app/'
     }
   },
   plugins: [
