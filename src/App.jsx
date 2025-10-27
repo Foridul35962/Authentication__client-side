f47b99501f7ceffa6b1b3b94abb3ee24ccfd732d
Foridul35962 <foridulislam35962@gmail.com>
2025-10-28 01:02:34 +0600

    navbar name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { CheckLogin } from './store/store'
 import axios from 'axios'
 
